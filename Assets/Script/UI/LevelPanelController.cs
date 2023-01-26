3d4f579c44d1d55d29884043aceb9c6a0e414af9
BlackBlank1 <121706904+BlackBlank1@users.noreply.github.com>
2023-01-26 11:45:27 +0800

    增加点击关卡右侧弹出关卡详情

--- Assets/Script/UI/LevelPanelController.cs
+++ Assets/Script/UI/LevelPanelController.cs
@@ -1,4 +1,5 @@
 using System;
+using DG.Tweening;
 using TS.Commons;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,20 +15,63 @@ namespace TS.UI
         [SerializeField]
         private Button back_button;
 
+        [SerializeField]
+        private RectTransform levelDetail;
+
+        [SerializeField]
+        private RectTransform scrollView;
+
+        [SerializeField]
+        private GameObject levelDetailPanel;
+
+        [SerializeField]
+        private Button battle_button;
+
+        [SerializeField]
+        private Button cancel_button;
+
+        private String sceneName = "HomeScene";
+
         private void Start()
         {
             level1_button.onClick.AddListener(() =>
             {
-                GameManager.Instance.LoadScene("Level1");
+                ShowLevelDetail();
+                sceneName = "Level1";
             });
             level2_button.onClick.AddListener(() =>
             {
-                GameManager.Instance.LoadScene("Level2");
+                ShowLevelDetail();
+                sceneName = "Level2";
             });
             back_button.onClick.AddListener(() =>
             {
                 gameObject.SetActive(false);
             });
+
+            battle_button.onClick.AddListener(() =>
+            {
+                GameManager.Instance.LoadScene(sceneName);
+            });
+
+            cancel_button.onClick.AddListener(() =>
+            {
+                HideLevelDetail();
+            });
+        }
+
+        public void HideLevelDetail()
+        {
+            levelDetail.DOAnchorPos(new Vector2(0, 0), 0.5f);
+            scrollView.DOAnchorPos(scrollView.pivot - levelDetail.pivot, 0.5f);
+            levelDetailPanel.SetActive(false);
+        }
+
+        public void ShowLevelDetail()
+        {
+            levelDetail.DOAnchorPos(new Vector2(-levelDetail.rect.width, 0), 0.5f);
+            scrollView.DOAnchorPos(scrollView.pivot - levelDetail.pivot + new Vector2(-levelDetail.rect.width/2, 0), 0.5f);
+            levelDetailPanel.SetActive(true);
         }
     }
 }
