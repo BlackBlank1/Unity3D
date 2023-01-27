8dd1268551b14b6dd42d03e8296c7ab3fa68035a
BlackBlank1 <121706904+BlackBlank1@users.noreply.github.com>
2023-01-27 14:07:28 +0800

    增加了关卡详情处的enemy图片展示

--- Assets/Script/UI/LevelPanelController.cs
+++ Assets/Script/UI/LevelPanelController.cs
@@ -29,20 +29,29 @@ namespace TS.UI
 
         [SerializeField]
         private Button cancel_button;
+        
+        [SerializeField]
+        private Image[] images;
+
+        [SerializeField]
+        private Sprite[] sourceImages;
 
         private String sceneName = "HomeScene";
 
         private void Start()
         {
+            //按钮设置与执行
             level1_button.onClick.AddListener(() =>
             {
                 ShowLevelDetail();
                 sceneName = "Level1";
+                ImagesSetting();
             });
             level2_button.onClick.AddListener(() =>
             {
                 ShowLevelDetail();
                 sceneName = "Level2";
+                ImagesSetting();
             });
             back_button.onClick.AddListener(() =>
             {
@@ -72,6 +81,30 @@ namespace TS.UI
             levelDetail.DOAnchorPos(new Vector2(-levelDetail.rect.width, 0), 0.5f);
             scrollView.DOAnchorPos(scrollView.pivot - levelDetail.pivot + new Vector2(-levelDetail.rect.width/2, 0), 0.5f);
             levelDetailPanel.SetActive(true);
+        }
+
+        public void ImagesSetting()
+        {
+            if (sceneName == "Level1")
+            {
+                for (int i = 0; i < images.Length - 1; i++)
+                {
+                    images[i].sprite = sourceImages[i];
+                }
+
+                images[images.Length-1].sprite = sourceImages[^1];
+            }
+            else if (sceneName == "Level2")
+            {
+                for (int i = 0; i < images.Length; i++)
+                {
+                    images[i].sprite = sourceImages[i];
+                }
+            }
+            else if (sceneName == "BossScene")
+            {
+                images[0].sprite = sourceImages[^1];
+            }
         }
     }
 }
